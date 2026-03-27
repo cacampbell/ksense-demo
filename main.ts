892c54fdb4a075f9087429eb5f35b5321a0f8c25
Chris Campbell <cacampbell@ucdavis.edu>
2026-03-27 14:46:17 -0700

    wip: get and parse one page of results, have to go make soup now
    Signed-off-by: Chris Campbell <cacampbell@ucdavis.edu>

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError, isAxiosError } from "axios";
 import { configDotenv } from "dotenv";
 
 const PatientGender = ["M", "F"] as const;
@@ -6,6 +6,8 @@ type PatientGender = typeof PatientGender[number];
 
 type PatientDiagnosis = string; // ?
 type PatientMedicationList = string; // ?
+
+type PatientBloodPressure = { systolic: number, diastoic: number };
 
 type ResponsePagination = {
     page: number;
@@ -35,13 +37,85 @@ interface PatientResponse {
     medications?: PatientMedicationList;
 }
 
+type Empty = null; // Could be a signifier as well
+type Patient = {
+    patient_id: string;
+    name: string | Empty;
+    age: number | Empty;
+    gender: PatientGender | Empty;
+    blood_pressure: PatientBloodPressure | Empty;
+    temperature: number | Empty;
+    visit_date: Date | Empty;
+    diagnosis: PatientDiagnosis | Empty;
+    medications: PatientMedicationList | Empty;
+}
+
 type PatientsResponse = {
     data: PatientResponse[];
     pagination: ResponsePagination;
     metadata: ResponseMetadata;
 }
 
+// Create api client
+const api = axios.create({
+    baseURL: "https://assessment.ksensetech.com/api/"
+})
+
 // Load API_KEY --> process.env.API_KEY
 configDotenv();
+const API_KEY = process.env.API_KEY;
+api.defaults.headers.common["x-api-key"] = API_KEY;
 
-console.log(process.env.API_KEY);
+// initial parameters
+const page = 1; // first page
+const limit = 20; // max per page
+const params = { page, limit };
+api.defaults.params = params;
+
+// parse blood pressure from combined string
+function parseBP(bloodPressureString: string): PatientBloodPressure {
+    const parts = bloodPressureString.split("/");
+    return {
+        systolic: Number.parseInt(parts[0]),
+        diastoic: Number.parseInt(parts[1])
+    }
+}
+
+// decontamination function that converts a PatientResponse to a Patient
+function convertPatientData(patientResponse: PatientResponse): Patient {
+    const patient = {
+        patient_id: patientResponse.patient_id,
+        name: patientResponse.name ?? null,
+        age: patientResponse.age ?? null,
+        gender: patientResponse.gender ?? null,
+        blood_pressure: patientResponse.blood_pressure ? parseBP(patientResponse.blood_pressure) : null,
+        temperature: patientResponse.temperature ?? null,
+        visit_date: patientResponse.visit_date ? new Date(patientResponse.visit_date) : null,
+        diagnosis: patientResponse.diagnosis ?? null,
+        medications: patientResponse.medications ?? null
+    }
+    return patient;
+}
+
+// function that gets all patient data once and converts it to domain objects
+async function getPatientData(): Promise<Patient[]> {
+    const results: Patient[] = [];
+    // const backoff = 30;
+
+    while (true) {
+        try {
+            const response = (await api.get<PatientsResponse>("patients")).data;
+            const intResults = response.data.map((patientResp: PatientResponse) => convertPatientData(patientResp));
+            console.log(intResults);
+            break;
+        } catch (error) {
+            if (isAxiosError(error)) {
+                
+            }
+        }
+    }
+
+    return results;
+}
+
+await getPatientData();
